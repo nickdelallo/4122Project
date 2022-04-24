0a3ac9af3233b4392b6e76969d6c37ab668ef17a
Nick DeLallo <55556910+nickdelallo@users.noreply.github.com>
2022-04-24 11:54:03 -0400

    Add files via upload

--- Netflix.py
+++ Netflix.py
@@ -1,28 +1,34 @@
+# imports
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import altair as alt
 from vega_datasets import data
 
+# data
 def load_data():
     df = pd.read_csv('Netflix.csv', index_col = 'Country_code')
     return df
 
+# title
 st.title('Netflix Data')
 
+# load
 df = load_data()
 
+# columns
 cols = df.columns
-# Columns
 col1, col2= st.columns(2)
-#option = st.multiselect('What sources do you want to display?', cols, cols[0])
-    
+
+# objective
 if st.sidebar.checkbox('Objective'):
     st.title("Optimal price based on library size")
     st.write(df)
-    
+
+# library size
 if st.sidebar.checkbox('Library Size'):
-        
+    
+    # movies
     movies = df.sort_values(['Movies'], ascending=False)
     movies = pd.concat([movies.head(10),movies.tail(10)]).sort_index()
     moviesChart = alt.Chart(movies).mark_bar().encode(
@@ -32,7 +38,8 @@ if st.sidebar.checkbox('Library Size'):
     ).properties(width=350)
     with col1:
         st.write(moviesChart)
-        
+    
+    # tv shows
     tvshows = df.sort_values(['TV_Shows'], ascending=False)
     tvshows = pd.concat([tvshows.head(10),tvshows.tail(10)]).sort_index()
     tvshowsChart = alt.Chart(tvshows).mark_bar().encode(
@@ -42,7 +49,8 @@ if st.sidebar.checkbox('Library Size'):
     ).properties(width=350)
     with col2:
         st.write(tvshowsChart)
-        
+    
+    # total
     totalLibrary = df.sort_values(['Total_Library_Size'], ascending=False)
     totalLibrary = pd.concat([totalLibrary.head(10),totalLibrary.tail(10)]).sort_index()
     totalLibraryChart = alt.Chart(totalLibrary).mark_bar().encode(
@@ -52,9 +60,11 @@ if st.sidebar.checkbox('Library Size'):
     ).properties(width=600)
     with col1:
         st.write(totalLibraryChart)
-        
+
+# cost
 if st.sidebar.checkbox('Cost'):
     
+    # basic
     basicCost = df.sort_values(['Cost_Per_Month_Basic'], ascending=False)
     basicCost = pd.concat([basicCost.head(10),basicCost.tail(10)]).sort_index()
     basicCostChart = alt.Chart(basicCost).mark_bar().encode(
@@ -65,6 +75,7 @@ if st.sidebar.checkbox('Cost'):
     with col1:
         st.write(basicCostChart)
         
+    # standard
     standardCost = df.sort_values(['Cost_Per_Month_Standard'], ascending=False)
     standardCost = pd.concat([standardCost.head(10),standardCost.tail(10)]).sort_index()
     standardCostChart = alt.Chart(standardCost).mark_bar().encode(
@@ -74,7 +85,8 @@ if st.sidebar.checkbox('Cost'):
     ).properties(width=350)
     with col2:
         st.write(standardCostChart)
-        
+      
+    # premium
     premiumCost = df.sort_values(['Cost_Per_Month_Premium'], ascending=False)
     premiumCost = pd.concat([premiumCost.head(10),premiumCost.tail(10)]).sort_index()
     premiumCostChart = alt.Chart(premiumCost).mark_bar().encode(
@@ -84,8 +96,16 @@ if st.sidebar.checkbox('Cost'):
     ).properties(width=350)
     with col1:
         st.write(premiumCostChart)
+        
+    with col2:
+        st.write('Basic: Not HD and only one screen')
+        st.write('Standard: HD and 2 screens')
+        st.write('Premium: Ultra HD and 4 screens')
 
+# findings
 if st.sidebar.checkbox('Findings'):
+    
+    # basic
     basicCostPerDollar = df
     basicCostPerDollar['result'] = basicCostPerDollar['Total_Library_Size']/basicCostPerDollar['Cost_Per_Month_Basic']
     basicCostPerDollar = basicCostPerDollar.sort_values(['result'], ascending=False)
@@ -98,5 +118,35 @@ if st.sidebar.checkbox('Findings'):
     with col1:
         st.write(basicCostPerDollarChart)
     
+    # standard
+    standardCostPerDollar = df
+    standardCostPerDollar['result'] = standardCostPerDollar['Total_Library_Size']/standardCostPerDollar['Cost_Per_Month_Standard']
+    standardCostPerDollar = standardCostPerDollar.sort_values(['result'], ascending=False)
+    standardCostPerDollar = pd.concat([standardCostPerDollar.head(10),standardCostPerDollar.tail(10)]).sort_index()
+    standardCostPerDollarChart = alt.Chart(standardCostPerDollar).mark_bar().encode(
+    x=alt.X('result:Q', title='Standard Cost Per $'),
+    y=alt.Y('Country:N', sort='-x', title='Country'),
+    color=alt.Color('result:Q')
+    ).properties(width=350)
+    with col2:
+        st.write(standardCostPerDollarChart)
+        
+    # premium
+    premiumCostPerDollar = df
+    premiumCostPerDollar['result'] = premiumCostPerDollar['Total_Library_Size']/premiumCostPerDollar['Cost_Per_Month_Premium']
+    premiumCostPerDollar = premiumCostPerDollar.sort_values(['result'], ascending=False)
+    premiumCostPerDollar = pd.concat([premiumCostPerDollar.head(10),premiumCostPerDollar.tail(10)]).sort_index()
+    premiumCostPerDollarChart = alt.Chart(premiumCostPerDollar).mark_bar().encode(
+    x=alt.X('result:Q', title='Premium Cost Per $'),
+    y=alt.Y('Country:N', sort='-x', title='Country'),
+    color=alt.Color('result:Q')
+    ).properties(width=350)
+    with col1:
+        st.write(premiumCostPerDollarChart)
+        
+    with col2:
+        st.write('Basic Mean: ' + str(round(basicCostPerDollar['result'].mean(), 2)))
+        st.write('Standard Mean: ' + str(round(standardCostPerDollar['result'].mean(), 2)))
+        st.write('Premium Mean: ' + str(round(premiumCostPerDollar['result'].mean(), 2)))
 if st.sidebar.checkbox('Solution'):
     st.write(df)
